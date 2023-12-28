7230975464bb659f8532f9bc4126b6af3165befa
Naseem Khan <nk8257949@gmail.com>
2023-12-29 00:27:22 +0500

    routing done

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,5 +1,26 @@
+import { Suspense, lazy } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const Customers = lazy(() => import("./pages/Customers"));
+const Products = lazy(() => import("./pages/Products"));
+const Transactions = lazy(() => import("./pages/Transactions"));
+
+import Loader from "./components/Loader";
+
 const App = () => {
-	return <div className="text-4xl text-emerald-500">App</div>;
+	return (
+		<Router>
+			<Suspense fallback={<Loader />}>
+				<Routes>
+					<Route path="/admin/dashboard" element={<Dashboard />} />
+					<Route path="/admin/customer" element={<Customers />} />
+					<Route path="/admin/product" element={<Products />} />
+					<Route path="/admin/transaction" element={<Transactions />} />
+				</Routes>
+			</Suspense>
+		</Router>
+	);
 };
 
 export default App;
